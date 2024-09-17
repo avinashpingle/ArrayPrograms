861b934ee1cb5b64ec67d9b2e1d3b912a58d90e5
avinashpingale <avinashpingale@gmail.com>
2024-09-17 17:15:15 +0530

    Adding second max program

--- src/com/testingshastra/FindSecondMax.java
+++ src/com/testingshastra/FindSecondMax.java
@@ -1,22 +1,36 @@
 package com.testingshastra;
 
-public class FindSecondMax {
+import java.util.Arrays;
+import java.util.TreeSet;
 
-	public static void main(String[] args) {
-		int[] x = {12,48,96,49,51,73,23,11,96};
-		int max = x[0], secondMax=0;
+public class FindSecondMax {
+	
+	public static void usingCollection(int[] arr) {
+		TreeSet<Integer> ts = new TreeSet<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			ts.add(arr[i]);
+		}
+		System.out.println("Second max: "+ts.lower(ts.last()));
+	}
+	
+	
+	public static void brutForce(int[] arr) {
 		
-		for (int i = 0; i < x.length; i++) {
-			if(x[i]>max) {
+		int max = arr[0], secondMax=0;
+		
+		for (int i = 1; i < arr.length; i++) {
+			if(arr[i]>max || arr[i]>secondMax) {
 				secondMax = max;
-				max = x[i];
-			}else if(x[i]>secondMax) {
-				secondMax = x[i];
+				max = arr[i];
 			}
 		}
 		
 		System.out.println("Second Max: "+secondMax);
-		System.out.println("Max: " + max);		
-		
+	}
+	
+	public static void main(String[] args) {
+		int[] x = {12,48,96,49,51,73,23,11,96};	
+		//brutForce(x);
+		usingCollection(x);
 	}
 }
